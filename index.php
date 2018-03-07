e12aaf7574f304f4314413e01e47cc8c0fc77cf7
luis.junior <luis.junior@51DE194277.senacrjedu.local>
2018-03-06 22:02:52 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+    Autor: Luis Paulo Jr
+    Local: Senac Rio
+    Data: 06-03-2018
+    Finalidade: Página principal do projeto
+-->
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 02</title>
+    </head>
+    <body>
+        <?php
+            $var1 = 10;
+            $var2 = 20;
+            $var3 = ($var1 + $var2);
+            
+            echo $var3. "<br>";
+        ?>
+    </body>
+</html>
