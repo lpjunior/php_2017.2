bfd3f6959cf9f0a8c6dc43d506f538cfedfac42a
luis.junior <luis.junior@51DE194277.senacrjedu.local>
2018-05-29 20:31:38 -0300

    Aula08

---
+++ src/poo/AppTeste.java
@@ -0,0 +1,30 @@
+package poo;
+
+public class AppTeste {
+	public static void main(String[] args) {
+		/*
+		 * Pessoa p - criação da referencia da Pessoa, aonde o 'p' é essa referencia.
+		 * 
+		 * new Pessoa() - criação da instancia(Objeto), aonde utilizamos a palavra new
+		 * para criar.
+		 */
+
+		/**
+		 * Comentário documental
+		 * 
+		 * @author Luis Paulo Jr.
+		 * @since 2018-05-29
+		 */
+
+		/**
+		 * @var p - Da acesso aos recursos da classe Pessoa
+		 */
+
+		Pessoa p = new Pessoa();
+
+		p.nome = "Claudemir";
+		p.sexo = "Masculino";
+		p.corDaPele = "Negro";
+		p.titulo = "Batman";
+	}
+}
--- src/poo/Pessoa.java
+++ src/poo/Pessoa.java
@@ -8,12 +8,13 @@ public class Pessoa {
 	 * Os atributos definem as caracteriscas de 
 	 * uma pessoa
 	 */
-	String nome;
-	String corDaPele;
-	Double altura;
-	Double peso;
-	Integer idade;
-	String sexo;
+	private String nome;
+	private String corDaPele;
+	private Double altura;
+	private Double peso;
+	private Integer idade;
+	private String sexo;
+	private String titulo;
 
 	/* Os métodos definem as ações de uma pessoa */
 	void andar() {}
@@ -21,4 +22,14 @@ public class Pessoa {
 	void comer() {}
 	String falar() { return ""; }
 	// void verificarLogin() {} -- não faz sentido
+	
+	// métodos gets - servem para resgatar a valor do atributo
+	public String getNome() {
+		return nome;
+	}
+	
+	// métodos sets - servem para atribuir(inserir) o valor do atributo
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
